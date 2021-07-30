4a41405b9cf2c5b4b3523691f784cbe92d7afa6c
Kyu_Il_Park <21800263@handong.edu>
2021-07-30 21:19:47 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import client from "./apollo";
+import{ ApolloProvider } from "@apollo/react-hooks";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
